10a004ea2a8f9ac82b743a91cdee2da872322fa5
kimzerovirus <yg9699k@naver.com>
2023-03-17 23:39:11 +0900

    project init

---
+++ src/test/kotlin/me/kzv/legacyboard/LegacyBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kzv.legacyboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LegacyBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
