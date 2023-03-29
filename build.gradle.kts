0cfa34534b39957fe570e99b10145fa9f759dbe8
kimzerovirus <yg9699k@naver.com>
2023-03-29 12:24:04 +0900

    h2 db -> mysql 로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.batch:spring-batch-test")
     testImplementation("org.springframework.security:spring-security-test")
