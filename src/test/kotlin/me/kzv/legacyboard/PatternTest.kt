ede71bf70d5f41afe681a60e27b0110a5fd33610
kimzerovirus <yg9699k@naver.com>
2023-03-30 14:12:59 +0900

    이미지 소스 추출 정규식 테스트

---
+++ src/test/kotlin/me/kzv/legacyboard/PatternTest.kt
@@ -0,0 +1,21 @@
+package me.kzv.legacyboard
+
+import java.util.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+
+fun main() {
+    var content = "<h1>extract img src test</h1> <p>hello world! <br/> <img src=\"www.naver.com/img.jpg\" alt=\"이미지 태그\"></p>"
+    for(i:Int in 1..10) content += content
+    extractImgSrc(content)
+}
+
+fun extractImgSrc(content: String) {
+    val pattern: Pattern = Pattern.compile("<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>")
+//        val pattern = Pattern.compile("<img[^>]*src=[\"']?([^>\"']+)[\"']?[^>]*>")
+    val matcher: Matcher = pattern.matcher(content)
+    while (matcher.find()) {
+        println(matcher.group(1))
+    }
+}
