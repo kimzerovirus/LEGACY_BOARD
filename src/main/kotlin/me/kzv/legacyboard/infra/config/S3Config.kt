8a400bc29fedc0a2cde8bc567866397ff93098b1
kimzerovirus <yg9699k@naver.com>
2023-06-29 01:36:10 +0900

    s3 config

---
+++ src/main/kotlin/me/kzv/legacyboard/infra/config/S3Config.kt
@@ -0,0 +1,35 @@
+package me.kzv.legacyboard.infra.config
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+// spring cloud 사용시 yml 설정만으로 bean 주입이 된다는데 bean 생성이 안됨..
+@Profile("prod")
+@Configuration
+class S3Config {
+    @Value("\${cloud.aws.credentials.access-key}")
+    lateinit var accessKey: String
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    lateinit var secretKey: String
+
+    @Value("\${cloud.aws.region.static}")
+    lateinit var region: String
+
+    @Bean
+    fun amazonS3Client(): AmazonS3 {
+        val credentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(AWSStaticCredentialsProvider(credentials))
+            .withRegion(region)
+            .build()
+    }
+}
